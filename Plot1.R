5c32282cfbeafbf1dbdbaea231546c3a4e4a65d3
vk2113 <50883983+vk2113@users.noreply.github.com>
2021-04-01 20:34:31 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,8 +1,11 @@
-# Set the Working Directory
+# Setting up the working directory
+
 setwd("~/GitHub/ExData_Plotting1")
 
 source("LoadData.R")
+
 png(filename = "Plot1.png", width = 480, height = 480, units = "px")
 hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", breaks = 12, ylim = c(0, 1200))
+
 dev.off()
 rm(data)
