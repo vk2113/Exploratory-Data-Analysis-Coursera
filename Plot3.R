c87e4af85fdfb1bb669acb45a534cdf5b76a017c
vk2113 <50883983+vk2113@users.noreply.github.com>
2021-04-01 20:36:20 +0530

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,7 +1,9 @@
-# Set the Working Directory
+# Setting up the working directory
+
 setwd("~/GitHub/ExData_Plotting1")
 
 source("LoadData.R")
+
 png(filename = "Plot3.png", width = 480, height = 480, units = "px")
 plot(Date_time, Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering", xaxt = "n")
 lines(Date_time, Sub_metering_1, col = "black")
@@ -9,5 +11,6 @@ lines(Date_time, Sub_metering_2, col = "red")
 lines(Date_time, Sub_metering_3, col = "blue")
 legend("topright", col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 1)
 axis(side = 1, at = c(1, 1441, 2880), labels = c("Thu", "Fri", "Sat"))
+
 dev.off()
 rm(data)
